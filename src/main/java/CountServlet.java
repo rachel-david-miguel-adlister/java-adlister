b2811143348eb87b1ec894f17167d01049e0cb2e
Justin Reich <justinreich.dev@gmail.com>
2020-01-08 13:17:38 -0600

    feat: Add exercise solution for Servlets

--- src/main/java/CountServlet.java
+++ src/main/java/CountServlet.java
@@ -3,16 +3,17 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet("/hello")
-public class HelloWorldServlet extends HttpServlet {
+@WebServlet("/count")
+public class CountServlet extends HttpServlet {
+
+    private static int count = 0;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.setContentType("text/html");
-        PrintWriter writer =  response.getWriter();
-        writer.println("<h1>Hello World</h1>");
+        count++;
+        response.getWriter().println(count);
+
     }
 
 
